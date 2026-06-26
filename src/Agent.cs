6da9edf59e47820b5e9f17ef715bb87e1dc627dd
RinoStack <rino44296@gmail.com>
2026-06-26 22:58:37 +0300

    chore: add src/Agent.cs

---
+++ src/Agent.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace OpenMonoAgent {
+    public class Agent {
+        public void Run() {
+            Console.WriteLine("OpenMonoAgent - Local AI Agent");
+        }
+    }
+}
